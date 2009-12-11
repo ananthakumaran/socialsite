d16d2d3a039f31102fbd84af1c4964f21c0dbac6
Anantha Kumaran <ananthakumaran@gmail.com>
2009-12-11 12:27:26 +0000

    wmd rich editor component added

---
+++ src/main/java/com/socialsite/util/wmd/RichEditor.java
@@ -0,0 +1,59 @@
+package com.socialsite.util.wmd;
+
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.markup.html.IHeaderContributor;
+import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.form.FormComponentPanel;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.model.IModel;
+
+/**
+ * WMD rich Editor
+ * 
+ * TODO convert the button image into a single image (CSS Sprite)
+ * 
+ * @author Ananth
+ */
+public class RichEditor<T> extends FormComponentPanel<T> implements
+		IHeaderContributor
+{
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	/** text area */
+	private TextArea<T>			textArea;
+
+	/**
+	 * constructor
+	 * 
+	 * @param id
+	 *            id
+	 * @param model
+	 *            model for the textarea
+	 */
+	public RichEditor(String id, IModel<T> model)
+	{
+		super(id, model);
+		add(textArea = new TextArea<T>("textarea", model));
+	}
+
+	protected void convertInput()
+	{
+		// TODO filter any javascript in the markup
+		setConvertedInput(textArea.getConvertedInput());
+	}
+
+	public void renderHead(IHeaderResponse response)
+	{
+		// jquery
+		response.renderJavascriptReference("js/jquery/jquery.min.js");
+		// TextArea Resizer
+		ResourceReference resizeRef = new ResourceReference(getClass(),
+			"jquery.textarearesizer.js");
+		response.renderJavascriptReference(resizeRef);
+		response.renderOnDomReadyJavascript("$('textarea').TextAreaResizer();");
+	}
+}
