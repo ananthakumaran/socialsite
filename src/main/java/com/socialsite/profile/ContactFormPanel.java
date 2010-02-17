21740cf3d63c52e264fefd8d50b1399590f0b6db
annathakumaran <ananthakumaran@gmail.com>
2010-02-16 21:05:19 -0800

    updating contact Tab

--- src/main/java/com/socialsite/profile/ContactFormPanel.java
+++ src/main/java/com/socialsite/profile/ContactFormPanel.java
@@ -24,12 +24,15 @@ import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.validation.validator.EmailAddressValidator;
 
 import com.socialsite.BasePanel;
 import com.socialsite.dao.ProfileDao;
 import com.socialsite.persistence.Profile;
+import com.socialsite.util.PrivacyField;
+import com.socialsite.util.PrivacyModel;
 
 /**
  * @author Ananth
@@ -53,9 +56,11 @@ public class ContactFormPanel extends BasePanel
 		Form<Profile> form = new Form<Profile>("form", new CompoundPropertyModel<Profile>(profile));
 		add(form);
 		form.add(new RequiredTextField<String>("email").add(EmailAddressValidator.getInstance()));
-		form.add(new TextField<String>("mobilePhone"));
-		form.add(new TextField<String>("landPhone"));
-		form.add(new TextField<String>("address"));
+		form.add(new PrivacyField("mobilePhone", new PropertyModel<PrivacyModel>(profile,
+				"mobilePhone")));
+		form.add(new PrivacyField("landPhone",
+				new PropertyModel<PrivacyModel>(profile, "landPhone")));
+		form.add(new PrivacyField("address", new PropertyModel<PrivacyModel>(profile, "address")));
 		form.add(new TextField<String>("city"));
 		form.add(new TextField<String>("neighborhood"));
 		form.add(new TextField<Integer>("zip"));
--- src/main/java/com/socialsite/profile/ContactInfoPanel.java
+++ src/main/java/com/socialsite/profile/ContactInfoPanel.java
@@ -17,7 +17,13 @@
 
 package com.socialsite.profile;
 
+import org.apache.wicket.model.Model;
+
 import com.socialsite.BasePanel;
+import com.socialsite.persistence.Profile;
+import com.socialsite.util.NonEmptyPanel;
+import com.socialsite.util.PrivacyModel;
+import com.socialsite.util.PrivacyPanel;
 
 /**
  * contains the contact info of the user
@@ -31,11 +37,28 @@ public class ContactInfoPanel extends BasePanel
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private Profile profile;
+
 
 	public ContactInfoPanel(String id)
 	{
 		super(id);
+		this.profile = getUser().getProfile();
 		setOutputMarkupId(true);
-	}
+		// should we show the email
+		add(new NonEmptyPanel("email", "Email", profile.getEmail()));
+		add(new PrivacyPanel("mobilePhone", new Model<PrivacyModel>(profile.getMobilePhone()),
+				"MobilePhone"));
+		add(new PrivacyPanel("landPhone", new Model<PrivacyModel>(profile.getMobilePhone()),
+				"LandPhone"));
+		add(new PrivacyPanel("address", new Model<PrivacyModel>(profile.getMobilePhone()),
+				"Address"));
+		add(new NonEmptyPanel("city", "City", profile.getCity()));
+		add(new NonEmptyPanel("neighborhood", "Neighborhood", profile.getNeighborhood()));
 
+		add(new NonEmptyPanel("zip", "Zip", (profile.getZip() == null) ? null : profile.getZip()
+				+ ""));
+		add(new NonEmptyPanel("website", "WebSite", profile.getWebsite()));
+
+	}
 }
