8f75838c52d64e78f796b79b7d1a877c0f60a486
annathakumaran <ananthakumaran@gmail.com>
2010-03-01 10:21:05 -0800

    activation page updated

--- src/main/java/com/socialsite/activation/ActivationPage.java
+++ src/main/java/com/socialsite/activation/ActivationPage.java
@@ -17,16 +17,78 @@
 
 package com.socialsite.activation;
 
+import java.util.Date;
+
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import com.socialsite.dao.ActivationDao;
+import com.socialsite.dao.UniversityDao;
+import com.socialsite.image.DefaultImage;
+import com.socialsite.persistence.Activation;
+import com.socialsite.persistence.University;
 
 /**
  * @author Ananth
  */
 public class ActivationPage extends WebPage
 {
+
+	private String messageText;
+	@SpringBean(name = "activationDao")
+	private ActivationDao activationDao;
+	@SpringBean(name = "universityDao")
+	private UniversityDao universityDao;
+
 	public ActivationPage(PageParameters params)
 	{
 
+		add(new Label("message", new PropertyModel<String>(this,"messageText")));
+		long id = params.getAsLong("id", 0);
+		if (id == 0)
+		{
+			messageText = "Access denied";
+		}
+		else
+		{
+			String action = params.getString("action");
+			Activation activation = activationDao.load(id);
+			if (activation != null)
+			{
+				if (action.equals("activate"))
+				{
+					createUniversity(activation);
+					sendNotification(activation, true);
+					messageText = "University Created";
+				}
+				else
+				{
+					sendNotification(activation, false);
+					messageText = "University request rejected";
+				}
+				activationDao.delete(activation);
+			}
+			else {
+				messageText = "activation id not found";
+			}
+		}
+	}
+
+	public void createUniversity(Activation activation)
+	{
+		University university = new University(activation.getUniversityName(), activation
+				.getAdmin());
+		university.setLastModified(new Date());
+		DefaultImage defaultImage = new DefaultImage();
+		defaultImage.forUniversity(university);
+		universityDao.save(university);
+	}
+
+	public void sendNotification(Activation activation, boolean accept)
+	{
+		// TODO send a notification message to the admin
 	}
 }
