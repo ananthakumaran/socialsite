5871c060672f501248134447aba5c88dcc57871f
annathakumaran <ananthakumaran@gmail.com>
2010-03-04 21:44:32 -0800

    fixing header

--- src/main/java/com/socialsite/BasePage.java
+++ src/main/java/com/socialsite/BasePage.java
@@ -79,13 +79,13 @@ public class BasePage extends WebPage implements IHeaderContributor
 		response.renderCSSReference("css/module/mod_skins.css");
 		response.renderCSSReference("css/talk/talk.css");
 		response.renderCSSReference("css/talk/talk_skins.css");
-//		response.renderCSSReference("css/global.css");
-//		response.renderCSSReference("css/home.css");
-//		response.renderCSSReference("css/login.css");
-//		response.renderCSSReference("css/scrap.css");
-//		response.renderCSSReference("css/profile.css");
-//		response.renderCSSReference("css/typography.css");
-//		response.renderCSSReference("css/round.css");
+		response.renderCSSReference("css/global.css");
+		response.renderCSSReference("css/home.css");
+		response.renderCSSReference("css/login.css");
+		response.renderCSSReference("css/scrap.css");
+		response.renderCSSReference("css/profile.css");
+		response.renderCSSReference("css/typography.css");
+		response.renderCSSReference("css/round.css");
 		response.renderCSSReference("css/wmd.css");
 		response.renderCSSReference("css/menu.css");
 
