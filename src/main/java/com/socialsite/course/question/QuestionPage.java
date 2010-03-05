d6e048c67346f2a6e31172a876e3bfbf1ef1798f
annathakumaran <ananthakumaran@gmail.com>
2010-03-04 18:18:46 -0800

    adding a seperate question page

---
+++ src/main/java/com/socialsite/course/question/QuestionPage.java
@@ -0,0 +1,45 @@
+/**
+ *     Copyright SocialSite (C) 2009
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.socialsite.course.question;
+
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+
+import com.socialsite.BasePage;
+import com.socialsite.course.answer.AddAnswerPanel;
+import com.socialsite.course.answer.AnswersPanel;
+import com.socialsite.persistence.Question;
+
+/**
+ * @author Ananth
+ */
+public class QuestionPage extends BasePage
+{
+
+	public QuestionPage(final IModel<Question> model)
+	{
+		final Question question = model.getObject();
+		add(new Label("heading", question.getHeading()));
+		add(new Label("text", question.getText()).setEscapeModelStrings(false));
+		MarkupContainer answerPanel;
+		add(answerPanel = new AnswersPanel("answers", model));
+		add(new AddAnswerPanel("addanswer", model, answerPanel));
+
+	}
+}
